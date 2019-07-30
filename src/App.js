56f5b36ed95404d80c3b100a2e79bc78275c97ff
Dieter Doman <dieter@jemstep.com>
2019-07-30 14:57:54 +0200

    Added the char component.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
 import ValidationComponent from "./ValidationComponent/ValidationComponent";
+import CharComponent from "./CharComponent/CharComponent";
 
 class App extends Component {
   state = {
-    wordCount: 0
+    text: ""
   };
 
   onInputChangeHandler = (event) => {
     const text = event.target.value;
-    this.setState({wordCount: text.length})
+    this.setState({text: text})
+  };
+
+  removeCharacterHandler = (index) => {
+      const text = this.state.text.split('');
+      text.splice(index, 1);
+      const newText = text.join('');
+      this.setState({text: newText});
   };
 
   render() {
+    const characters = this.state.text.split('');
+    const characterComponents = characters.map((character, index) => {
+        return (
+            <CharComponent key={index} click={() => this.removeCharacterHandler(index)} character={character}/>
+        );
+    });
+
     return (
       <div className="App">
-        <input type="text" onChange={this.onInputChangeHandler}/>
-        <p>{this.state.wordCount}</p>
-        <ValidationComponent textLength={this.state.wordCount}/>
-        <ol>
-          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
-          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
-          <li>When you click a CharComponent, it should be removed from the entered text.</li>
-        </ol>
-        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
+        <input type="text" onChange={this.onInputChangeHandler} value={this.state.text}/>
+        <p>{this.state.text.length}</p>
+        <ValidationComponent textLength={this.state.text.length}/>
+        {characterComponents}
       </div>
     );
   }
---
+++ src/CharComponent/CharComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './CharComponent.css';
+
+const CharComponent = (props) => {
+    return (<div className="CharComponent" onClick={props.click}>{props.character}</div>)
+};
+
+export default CharComponent;
